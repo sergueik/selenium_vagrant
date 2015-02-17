b6b301d3ef9812d22cbbf77e50cebab3e2e0844c
sergueik <kouzmine_serguei@yahoo.com>
2015-02-17 08:28:07 -0500

    removed consolidated cookbook

--- cookbooks/selenium/metadata.rb
+++
@@ -1,5 +0,0 @@
-name              'selenium'
-description       'Installs xvfb or vncsever jenkins and starts both hub and node.'
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '0.0.1'
-
