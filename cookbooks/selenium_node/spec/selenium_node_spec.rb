ab564fcf8cc9bf4d22c7a3ebbd4ffc5cbd9ea0b5
sergueik <kouzmine_serguei@yahoo.com>
2015-02-23 18:32:34 -0500

    validating json

--- cookbooks/selenium_node/spec/selenium_node_spec.rb
+++ cookbooks/selenium_node/spec/selenium_node_spec.rb
@@ -18,18 +18,9 @@ describe 'selenium_node::default' do
   end
 
   it 'generates a valid json configuration' do
-    expect(chef_run).to render_file(@json_file).with_content(satisfy do |content| 
-begin
-parsed_content = JSON.parse(content) 
-rescue 
-nil
-end
-
-end
- )
+    expect(chef_run).to render_file(@json_file).with_content(satisfy { |content|
+    JSON.parse(content) rescue nil })
   end 
-# https://github.com/opscode-cookbooks/chef-vault
-# http://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&ved=0CCAQFjAA&url=http%3A%2F%2Fapidock.com%2Fruby%2FString%2Fstart_with%253F&ei=cqDrVNGSIcLvoATK-IKIDw&usg=AFQjCNFyA3--YZzUgNZmbQsSnIIAgXoY0g&sig2=hYh25nwPSO-umllt4V6hlA&bvm=bv.86475890,d.cGU
 end
 
 
