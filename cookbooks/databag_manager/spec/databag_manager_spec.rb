1dbeb29967ff028318b095feb94eb8e9437d4dfa
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-02-24 08:29:19 -0500

    added databad application mockup data

--- cookbooks/databag_manager/spec/databag_manager_spec.rb
+++ cookbooks/databag_manager/spec/databag_manager_spec.rb
@@ -6,52 +6,87 @@ require 'chef-vault'
 describe 'databag_manager::default' do
 
   before(:each) do
-    @databag_manager_name = 'spec-vault'
+    @environment = 'spec'
+    @vault_databag = 'spec-vault'
     @seedvalue = 12345
     @dbweb = 'xxxx'
     @dbdwh = 'yyy'
     @rpm_version = 42
-    stub_data_bag(@databag_manager_name).and_return({'seedvalue' => nil, 'dbweb-enc' =>nil })
+    @application_server = 'hal-giftsf-server'
+    @server_config_databag = 'spec_tomcat_app_config'
+    @user_account = nil
+    @group_account = nil
+    @http_port = nil
+    @https_port = nil
+    @jmx_port = nil
+    @ajp_port = nil
+    @shutdown_port = nil
 
-    stub_data_bag_item("spec_tomcat_app_config",'hal-giftsf-server').and_return({'spec' => { 'application' => { 'user'=> nil, 'group'=> nil, 'rpm_version'=> @rpm_version } } })
+    stub_data_bag(@vault_databag).and_return(
+        {
+            'seedvalue' => nil,
+            'dbweb-enc' => nil
+        }
+    )
+    stub_data_bag_item(@server_config_databag, @application_server).and_return(
+        {'spec' => {
+            'application' => {
+                'user' => @user_account,
+                'group' => @group_account,
+                'rpm_version' => @rpm_version,
+                'ports' => { 
+                            'http' => @http_port,
+                            'http' => @http_port,
+                            'https' => @https_port,
+                            'jmx' => @http_port,
+                            'ajp' => @ajp_port,
+                            'shutdown' => @shutdown_port
+                            }
+	        }
+            }
+        }
+    )
   end
 
 
   let(:chef_run) do
-    # Stub any calls to Environment
     ChefSpec::SoloRunner.new do |node|
+
+      # Stub any calls to Environment
       env = Chef::Environment.new
-      env.name 'spec'
+      env.name @environment
       allow(node).to receive(:chef_environment).and_return(env.name)
       allow(Chef::Environment).to receive(:load).and_return(env)
 
       # Stub any calls to ChefVault
-      #   chefvault_item_double = double("ChefVault::Item")
-      # expect does not get fired
-      # expect(chefvault_item_double).to receive(:load).with(@databag_manager_name,'seedvalue').and_return({"seedvalue"=>nil})
-      allow(ChefVault::Item).to receive(:load).with(@databag_manager_name,'seedvalue').and_return({"seedvalue"=>@seedvalue})
-      allow(ChefVault::Item).to receive(:load).with(@databag_manager_name,'dbweb-enc').and_return({"dbweb"=>@dbweb})
-      allow(ChefVault::Item).to receive(:load).with(@databag_manager_name,'dbdwh').and_return({"dbdwh"=>@dbdwh})
+      # NOTE: expect does not get work
+      allow(ChefVault::Item).to receive(:load).with(@vault_databag, 'seedvalue').and_return(
+                                    {
+                                        'seedvalue' => @seedvalue
+                                    })
+      allow(ChefVault::Item).to receive(:load).with(@vault_databag, 'dbweb-enc').and_return(
+                                    {
+                                        'dbweb' => @dbweb
+                                    })
+      allow(ChefVault::Item).to receive(:load).with(@vault_databag, 'dbdwh').and_return(
+                                    {
+                                        'dbdwh' => @dbdwh
+                                    })
+
     end.converge(described_recipe)
   end
 
-  # TODO expectation to not receive the exception Chef::Exceptions::InvalidDataBagPath:
-  it 'finds seedvalue data in the vault databag' do
-    expect(chef_run).to write_log( /#{@seedvalue}/ )
+  # TODO code expectation to not receive the exception Chef::Exceptions::InvalidDataBagPath
+  it 'reads configuration data in the vault databag' do
+    expect(chef_run).to write_log(/#{@seedvalue}/)
+    expect(chef_run).to write_log(/#{@dbdwh}/)
+    expect(chef_run).to write_log(/#{@dbweb}/)
   end
 
-  it 'finds dbdwh data in the vault databag' do
-    expect(chef_run).to write_log( /#{@dbdwh}/ )
+  it 'reads application data in the tomcat_app_config databag' do
+    expect(chef_run).to write_log(/#{@user_account}/)
+    expect(chef_run).to write_log(/#{@group_account}/)
+    expect(chef_run).to write_log(/#{@rpm_version}/)
   end
-
-  it 'finds dbweb data in the vault databag' do
-    expect(chef_run).to write_log( /#{@dbweb}/ )
-  end
-
-  it 'finds rpm_version data in the tomcat_app_config databag' do
-    expect(chef_run).to write_log( /#{@rpm_version}/ )
-  end
-
 
 end
-
