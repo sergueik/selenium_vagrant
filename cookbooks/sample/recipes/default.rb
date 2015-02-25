b451949ad75fc6eb42817912afad6799a2106ee0
sergueik <kouzmine_serguei@yahoo.com>
2015-02-25 17:59:13 -0500

    added sample recipes

--- cookbooks/sample/recipes/default.rb
+++ cookbooks/sample/recipes/default.rb
@@ -2,27 +2,50 @@ log 'Starting Powershell script' do
   level :info
 end
 
-# This will fail when the user is unauthorized  to registry access:
-# STDERR: Set-ExecutionPolicy : Access to the registry key 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell' is denied.
-# At C:\Users\sergueik\AppData\Local\Temp\chef-script20150225-10904-tugust.ps1:2 char:2
 
-powershell "Enable execution of PowerShell scripts" do
+powershell 'Hello World from PowerShell scripts' do
   code <<-EOH
- write-output 'This is a test'
- EOH
+  write-output 'This is a test'
+   EOH
 end
 
-powershell "Enable execution of PowerShell scripts for x86" do
+# This may fail when the user is unauthorized  to registry access therefore needs to be somehow excluded from lone chef-solo runs:
+# STDERR: Set-ExecutionPolicy : Access to the registry key 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell' is denied.
+# At C:\Users\sergueik\AppData\Local\Temp\chef-script20150225-10904-tugust.ps1:2 char:2
+powershell "Enable execution of PowerShell scripts." do
   code <<-EOH
  Set-ExecutionPolicy -ExecutionPolicy remotesigned -force -scope LocalMachine
  EOH
 end
 
-powershell "This will a failing script" do
+powershell "This will be a failing script" do
   code <<-EOH
- Set-Executiowzsaewfd43w5r
+ try {
+  Set-Executiowzsaewfd43w5r
+ } catch [Exception] { 
+ Write-Output (($_.Exception.Message) -split "`n")[0]
+}
  EOH
 end
+
+powershell 'This will attempt to show  message box and probably fail' do
+  code <<-EOH
+@('System.Drawing','System.Windows.Forms') |  foreach-object {   [void] [System.Reflection.Assembly]::LoadWithPartialName($_) } 
+ [System.Windows.Forms.MessageBox]::Show('this is a test' )
+ EOH
+end
+
+Begin output of 
+C:\Windows\system32\WindowsPowershell\v1.\powershell.exe -NoLogo -NonInteractive -NoProfile -ExecutionPolicy RemoteSigned -InputFormat None -Command "C:\Users\vagrant\AppData\Local\Temp\chef-script20150225-3912-23ife0.ps1" 
+# ----
+# STDOUT: 
+# STDERR: Exception calling "Show" with "1" argument(s): 
+# Showing a modal dialog box or form when 
+# the application is not running in UserInteractive mode is not a valid operation. 
+# Specify the ServiceNotification or DefaultDesktopOnly style 
+# to display a notification from a service application.
+# 
+
 
 #
 #windows_batch "Enable execution of PowerShell scripts for x86" do
