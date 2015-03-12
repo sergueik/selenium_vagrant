1247f6617f30bb8f97b5ee4966306b9e35acf939
sergueik <kouzmine_serguei@yahoo.com>
2015-03-12 19:47:13 -0400

    initial version to handle sut_hosts

--- cookbooks/wrapper_hostsfile/recipes/default.rb
+++ cookbooks/wrapper_hostsfile/recipes/default.rb
@@ -1,20 +1,28 @@
 log 'configuring hosts' do
   level :info
 end
+
 append_rest_to_first = false
-loopback_hostnames = [
-    'metrics.carnival.com',
-    'smetrics.carnival.com',
-    'metrics.carnival.co.uk',
-    'smetrics.carnival.co.uk',
-    'static.ak.facebook.com',
-    's-static.ak.facebook.com',
-    'ad.doubleclick.net',
-    'ad.yieldmanager.com',
-    'pc1.yumenetworks.com',
-    'fbstatic-a.akamaihd.net',
-    'ad.amgdgt.com'
-  ]
+sut_hosts = {
+# TODO: handle aliases 
+ '172.26.5.51' =>   'www.carnival.com',
+ '172.26.5.51' =>   'origin-www.carnival.com',
+ '172.26.5.56' =>   'www.carnival.co.uk'
+}
+
+loopback_hostnames = %w/
+    metrics.carnival.com
+    smetrics.carnival.com
+    metrics.carnival.co.uk
+    smetrics.carnival.co.uk
+    static.ak.facebook.com
+    s-static.ak.facebook.com
+    ad.doubleclick.net
+    ad.yieldmanager.com
+    pc1.yumenetworks.com
+    fbstatic-a.akamaihd.net
+    ad.amgdgt.com
+  /
 if append_rest_to_first
   hostsfile_entry '127.0.0.1' do
     hostname  loopback_hostnames[0]
@@ -29,14 +37,13 @@ else
     end
    end
 end
-
-
-# TODO 
-# 172.26.5.51     www.carnival.com
-# 172.26.5.51     origin-www.carnival.com
-# 172.26.5.56     www.carnival.co.uk
-
-
+sut_hosts.each do |sut_ip,sut_hostname| 
+  hostsfile_entry sut_ip  do
+  hostname sut_hostname
+  unique true
+  action :create_if_missing
+  end 
+end
 log 'Finished configuring hostfiles.' do
   level :info
 end
