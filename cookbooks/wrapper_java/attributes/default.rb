960c22c291f3fe0aa1b3eb3fe937f5e6c9832157
sergueik <kouzmine_serguei@yahoo.com>
2015-04-14 08:28:44 -0400

    enforced the version to prevent using outdated cookie

--- cookbooks/wrapper_java/attributes/default.rb
+++ cookbooks/wrapper_java/attributes/default.rb
@@ -1,4 +1,5 @@
 # default jdk attributes
-default['java']['install_flavor'] = "openjdk"
-default['java']['jdk_version'] = '6'
-default['java']['arch'] = kernel['machine'] =~ /x86_64/ ? "x86_64" : "i586"
+default['java']['install_flavor'] = 'oracle'
+default['java']['jdk_version'] = '7'
+default['java']['arch'] = kernel['machine'] =~ /x86_64/ ? 'x86_64' : 'i586'
+default['java']['oracle']['accept_oracle_download_terms'] = true
--- cookbooks/wrapper_java/metadata.rb
+++ cookbooks/wrapper_java/metadata.rb
@@ -1,8 +1,9 @@
 name             'wrapper_java'
 maintainer       'Serguei Kouzmine'
-maintainer_email 'simon_holdsworth@uk.ibm.com'
+maintainer_email 'koumine_serguei@yahoo.com'
 description      'Installs/Configures Java using cookbook wrapper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.1'
-supports         'ubuntu', '= 12.04'
-depends	'java'
+supports         'ubuntu', '>= 12.04'
+supports         'centos'
+depends	         'java' ,'>= 1.31'
