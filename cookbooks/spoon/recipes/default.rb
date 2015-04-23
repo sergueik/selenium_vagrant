35960c4b9486f980cc6d5d1094171d89269b4182
sergueik <kouzmine_serguei@yahoo.com>
2015-04-22 20:49:15 -0400

    fixed spoon cookbook

--- cookbooks/spoon/recipes/default.rb
+++ cookbooks/spoon/recipes/default.rb
@@ -9,328 +9,76 @@ dest_file = 'spoon-plugin.exe'
 job_xml   = 'install_spool_plugin.xml'
 temp_path = 'C:\\temp'
 dest_file_path = "#{temp_path}/#{dest_file}"
-shared_folder = '\\\\VBOXSVR\\v-csdb-2'
+shared_folder = '\\\\VBOXSVR\\v-csdb-2'  # use data-bags share
 sample_image_tag = 'spoonbrew/base:1'
 import_shared_images = false
 import_browser_images = false
 
-spoon_shared_images = %w|
-    spoonbrew%2Fbase%3A1
-    gnu%2Fwget
-    oracle%2Fjre-core%3A8.25
-    selenium-server-standalone%3A2.43
-    selenium-chrome-driver
-    selenium-ie-driver
-    selenium-grid-plugin%3A201502271240
-|
-
-spoon_browser_images = %w|
-    spoonbrew%2Fie-selenium%3A9
-    spoonbrew%2Fie-selenium%3A10
-    spoonbrew%2Fie-selenium%3A11
-|
-
-log "Starting Download #{dest_file}" do
-  level :info
-end
-
-# Create temp folder
-directory temp_path  do
-  action :create
-end
-
-powershell "Download Spoon Plugin #{source_url}" do
-  code <<-EOH
-
-  [string]$username = '#{username}'
-  [string]$source_url = '#{source_url}'
-  [string]$dest_file = '#{dest_file}'
-  [string]$dest_file_path = '#{dest_file_path}'
-  [string]$password = '#{password}'
-  [bool]$use_proxy = $false
-
-# http://poshcode.org/1942
-function assert {
-  [CmdletBinding()]
-  param(
-    [Parameter(Position = 0,ParameterSetName = 'Script',Mandatory = $true)]
-    [scriptblock]$Script,
-    [Parameter(Position = 0,ParameterSetName = 'Condition',Mandatory = $true)]
-    [bool]$Condition,
-    [Parameter(Position = 1,Mandatory = $true)]
-    [string]$message)
-
-  $message = "ASSERT FAILED: $message"
-  if ($PSCmdlet.ParameterSetName -eq 'Script') {
-    try {
-      $ErrorActionPreference = 'STOP'
-      $success = & $Script
-    } catch {
-      $success = $false
-      $message = "$message`nEXCEPTION THROWN: $($_.Exception.GetType().FullName)"
-    }
-  }
-  if ($PSCmdlet.ParameterSetName -eq 'Condition') {
-    try {
-      $ErrorActionPreference = 'STOP'
-      $success = $Condition
-    } catch {
-      $success = $false
-      $message = "$message`nEXCEPTION THROWN: $($_.Exception.GetType().FullName)"
-    }
-  }
-
-  if (!$success) {
-    throw $message
-  }
-}
-
-
-Write-Host 'Probing credentials'
-
-  [system.Net.WebRequest]$request = [system.Net.WebRequest]::Create($source_url)
-  try {
-    [string]$encoded = [System.Convert]::ToBase64String([System.Text.Encoding]::GetEncoding('ASCII').GetBytes(($username + ':' + $password)))
-    $request.Headers.Add('Authorization','Basic ' + $encoded)
-  } catch [argumentexception]{
-
-  }
-  # TODO -  statuscode
-  if ($PSBoundParameters['use_proxy']) {
-
-    # Use current user NTLM credentials do deal with corporate firewall
-    $proxy_address = (Get-ItemProperty 'HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings').ProxyServer
-
-    if ($proxy_address -eq $null) {
-      $proxy_address = (Get-ItemProperty 'HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings').AutoConfigURL
-    }
-
-    if ($proxy_address -eq $null) {
-      # write a hard coded proxy address here 
-      $proxy_address = 'http://proxy.carnival.com:8080/array.dll?Get.Routing.Script'
-    }
-
-    $proxy = New-Object System.Net.WebProxy
-    $proxy.Address = $proxy_address
-    Write-Host ('Probing {0}' -f $proxy.Address)
-    $proxy.useDefaultCredentials = $true
-
-
-    Write-Host ('Use Proxy: "{0}"' -f $proxy.Address)
-    $request.proxy = $proxy
-    $request.useDefaultCredentials = $true
-  }
-<#
-  $sleep_interval = 10
-  $max_retries = 5
-
-  for ($i = 0; $i -ne $max_retries; $i++) {
-
-    Write-Host ('Try {0}' -f $i)
-
-    try {
-#>
-     $response = $request.GetResponse()
-
-assert -Script { ($response.StatusCode -eq 'OK') } -Message ( 'bad response status code: {0}' -f $response.StatusCode )
-<#
- NOTE: when using Download button from  https://spoon.net, Linux host platform is rejected with the dialog:
- "Spoon applications currently only launch on Windows PCs and tablets, 
- but support for Mac is coming soon! "
-#>
-Write-Host "Downloading ${dest_file}"
-
-$webclient = new-object System.Net.WebClient
-$credCache = new-object System.Net.CredentialCache
-$creds = new-object System.Net.NetworkCredential($username,$password)
-$credCache.Add($source_url, "Basic", $creds)
-$webclient.Credentials = $credCache
-[void]((New-Object Net.WebClient).DownloadFile($source_url,$dest_file_path))
-
-<#
-NOTE: cannot use https:
-
-"The underlying connection was closed: Could not establish trust relationship for the SSL/TLS secure channel."
-
-#>
-  EOH
-  not_if { ::File.exists?(dest_file_path) }
-end
-
-log 'Complete spoon-plugin Download' do
-  level :info
-end
-
-
-template ("#{temp_path}/#{job_xml}") do 
-  source 'install_spoon_plugin.erb'
-  variables(
-    :account_username => account_username,
-    :account_userdomain => account_userdomain,
-    :program => dest_file, 
-    :path  => temp_path 
-    ) 
- action :create_if_missing
-end 
-
-powershell "Install Spoon Plugin #{dest_file}" do
-  
-code <<-EOH
-
-$name = 'Install spoon plugin'
-& schtasks /Delete /F /TN $name
-write-output "Starting task '${name}'"
-
-& schtasks /Create /TN $name /XML "#{temp_path}\\#{job_xml}"
-& schtasks /Run /TN $name
-write-output "Waiting for status of the task '${name}'"
-start-sleep -second 1
-
-while($true){
-  $status = schtasks /query /TN $name| select-string -pattern "${name}"
-  write-output $status
-  if ($status.tostring() -match '(Running|Ready)'){
-    write-host "${name} is running..."
-    break 
-  } else { 
-    write-host "${name} is not yet running..."
-  }
-  start-sleep -milliseconds 1000
-}
-
-while($true){
-  $status = schtasks /query /TN $name|select-string  -pattern "${name}"
-  if ($status.tostring() -match 'Ready'){
-    write-host "${name} is ready."
-    break 
-  } else { 
-    write-host "${name} is not yet ready."
-  }
-  start-sleep -milliseconds 1000
-}
-  & schtasks /Delete /F /TN $name
-
-
-  EOH
-  only_if { ::File.exists?( "#{temp_path}/#{job_xml}" ) }
-  # skip if spoon is installed
-  not_if { ::Registry.value_exists?('HKCU\Software\Code Systems\Spoon','Id')}
-end
-
-powershell "Pulling Spoon Image: #{sample_image_tag}" do
+powershell "Logon to spoon" do
   code <<-EOH
   $env:PATH="${env:PATH};C:\\Program Files\\Spoon\\Cmd"
-  & spoon.exe help
   & spoon.exe login #{username} "#{password}"
-  & spoon.exe pull #{sample_image_tag}
   EOH
-  only_if  { ::File.exists?( "#{shared_folder}" ) }
-end
-# Store URL-encoded image names in the filenames
-# TODO provide a guard 
-# not_if "spoon.exe images --no-truncace | findstr '#{spoon_image_tag}' "
-if import_shared_images
-  spoon_shared_images.each do |spoon_box_image|
-    spoon_image_tag = spoon_box_image.gsub('%3A',':').gsub('%2F','/')
-    powershell "Importing Spoon Image: #{spoon_image_tag}" do
-      code <<-EOH
-      & spoon.exe login #{username} "#{password}"
-      & spoon.exe import --name=#{spoon_image_tag} --overwrite svm #{shared_folder}\\#{spoon_box_image}
-      EOH
-      only_if  { ::File.exists?( "#{shared_folder}\\#{spoon_box_image}" ) }
-    end
-  end
+  # only if spoon-plugin is installed
+  only_if  { ::Registry.value_exists?('HKCU\Software\Code Systems\Spoon','Id')}
 end
 
-if import_browser_images
-  spoon_browser_images.each do |spoon_box_image|
-    spoon_image_tag = spoon_box_image.gsub('%3A',':').gsub('%2F','/')
-    powershell "Importing Spoon Image: #{spoon_image_tag}" do
-      code <<-EOH
-      & spoon.exe login #{username} "#{password}"
-      & spoon.exe import --name=#{spoon_image_tag} --overwrite svm #{shared_folder}\\#{spoon_box_image}
-      EOH
-      only_if  { ::File.exists?( "#{shared_folder}\\#{spoon_box_image}" ) }
-    end
-  end
-end
+# removed download and install selenium-plugin.exe and pull and import images: repackaged the box with the spoon images already present.
+# Get-ScheduledTask relies on underlying features of the OS that Windows 7 doesn't have, 
+# so there is no way to run the cmdlet on that OS, even with PowerShell v4.
+# http://stackoverflow.com/questions/26658249/powershell-4-get-scheduledtask-and-windows
+
+
 # https://technet.microsoft.com/en-us/library/cc725744.aspx
 
 # NOTE: batch resource requires Chef 11.6.0 or later
 # The box image used has chef-windows-10.34.6-1.windows
-# --detach  does not seem to work
-# https://technet.microsoft.com/en-us/library/dd347721.aspx
-# https://github.com/chef/chef/issues/2348
-powershell 'Launch selenium-grid' do
-  run_command = "'C:\\Program Files\\Spoon\\Cmd\\spoon.exe' run base,selenium-grid"
-  taskname = 'Launch_selenium_grid'
-  log_filename = "c:/temp/#{taskname}.log"
+# TODO spoon stop <running containers>
+powershell  'Launch selenium-grid' do
+  spoon_command = 'run base,spoonbrew/selenium-grid'
+  run_command = "'C:\\Program Files\\Spoon\\Cmd\\spoon.exe' #{spoon_command}"
+  taskname = 'Launch_selenium_grid_node'
   code <<-EOH
-$custom_log = "c:/temp/#{taskname}-custom.log"
-Start-Transcript -Path #{log_filename} 
+
 $level = 'HIGHEST'
 $schedule = 'ONCE'
-$time = '00:00' 
-<#
-C:\Users\vagrant>spoon.exe run base,selenium-grid
-Upgrading Spoon VM to version 11.7.11
-Error: Pulling image failed
-#>
+$time = '00:00' # required, irrrevant
 $run_command = "#{run_command}"
 $taskname = '#{taskname}'
-
-write-output "`& SchTasks.exe /Delete /TN ${taskname} /F" | Tee-Object -FilePath  $custom_log 
-& SchTasks.exe /Delete /TN $taskname /F
-
-
-write-output "`& SchTasks.exe /Create  /TN ${taskname} /RL ${level} /TR ${run_command} /SC ${schedule} /ST ${time}" | Tee-Object -FilePath  $custom_log 
+<#
+  $status = schtasks /query /TN $taskname| select-string -pattern "${taskname}"
+  write-output $status
+  if ($status -ne $null){
+    write-host "${taskname} is present, deleting..."
+   & SchTasks.exe /Delete /TN $taskname /F
+    break 
+  } else { 
+    write-host "No ${taskname} is present..."
+    break 
+  }
+#>
 & SchTasks.exe /Create  /TN $taskname /RL $level /TR $run_command /SC $schedule /ST $time
-write-output "`& SchTasks.exe /Run /TN ${taskname}" | Tee-Object -FilePath  $custom_log 
 & SchTasks.exe /Run /TN $taskname
-
-write-output "Waiting for status of the task '${taskname}'" | Tee-Object -FilePath  $custom_log 
-
-start-sleep -second 1
 
 while($true){
   $status = schtasks /query /TN $taskname| select-string -pattern "${taskname}"
-  write-output "schtasks /query /TN $taskname| select-string -pattern `"${taskname}`""  | Tee-Object -FilePath  $custom_log 
-  write-output $status | Tee-Object -FilePath  $custom_log 
-
+  write-output $status
   if ($status.tostring() -match '(Running|Ready)'){
-
-    write-output "${taskname} is running..."| Tee-Object -FilePath  $custom_log 
+    write-host "${taskname} is running..."
     break 
   } else { 
-    write-host "${taskname} is not yet running..."| Tee-Object -FilePath  $custom_log 
+    write-host "${taskname} is not yet running..."
   }
   start-sleep -milliseconds 1000
 }
-Stop-Transcript
   EOH
   action  :run
 end
 
-ruby_block "Check Log" do
-  taskname = 'Launch_selenium_grid'
-  log_filename = "c:/temp/#{taskname}.log"
-  block do
-    version = ""
-
-    if File.exists?(log_filename)
-      # Read the log file.
-      f = File.open(log_filename)
-      f.each do  |line|
-        Chef::Log.info(line)
-        end
-      f.close
-    end
-  end
-end
-
-powershell  'Launch selenium-grid-node' do
-  run_command = "'C:\\Program Files\\Spoon\\Cmd\\spoon.exe' run base,spoonbrew/ie-selenium:9,selenium-grid-node node ie 9"
-  taskname = 'Launch_selenium_grid_node'
+powershell  'Launch selenium-grid-node ie 10' do
+  spoon_command = 'run base,spoonbrew/ie-selenium:10,spoonbrew/selenium-grid-node node ie 10'
+  run_command = "'C:\\Program Files\\Spoon\\Cmd\\spoon.exe' #{spoon_command}"
+  taskname = 'Launch_selenium_grid_node_ie_10'
   code <<-EOH
 
 $level = 'HIGHEST'
@@ -340,7 +88,18 @@ $time = '00:00' # required, irrrevant
 $run_command = "#{run_command}"
 $taskname = '#{taskname}'
 
-& SchTasks.exe /Delete /TN $taskname /F
+<#
+  $status = schtasks /query /TN $taskname| select-string -pattern "${taskname}"
+  write-output $status
+  if ($status -ne $null){
+    write-host "${taskname} is present, deleting..."
+   & SchTasks.exe /Delete /TN $taskname /F
+    break 
+  } else { 
+    write-host "No ${taskname} is present..."
+    break 
+  }
+#>
 & SchTasks.exe /Create  /TN $taskname /RL $level /TR $run_command /SC $schedule /ST $time
 & SchTasks.exe /Run /TN $taskname
 
@@ -356,7 +115,6 @@ while($true){
   start-sleep -milliseconds 1000
 }
 
-
   EOH
   action  :run
 end
@@ -364,4 +122,12 @@ end
 log 'Completed install and launch Spoon Selenium Grid.' do
   level :info
 end
+
+# http://www.msfn.org/board/topic/143463-prevent-password-expiration/
+# wmic path Win32_UserAccount WHERE Name='MyUserName' set PasswordExpires=true
+# net accounts /maxpwage:90
+# net user [username] /expires:06/30/11
+
+# $EXPIRY = gwmi win32_USERACCOUNT | Where-Object {$_.NAME -eq "MrJinje"};$EXPIRY.PasswordExpires = $False;$EXPIRY.Put()
+# direct execution hangs
 
