bad171bf6e2450002a16670a6dc2afc57f71a359
sergueik <kouzmine_serguei@yahoo.com>
2015-07-22 19:53:48 -0400

    added wrapper for google-chrome

---
+++ cookbooks/wrapper_google-chrome/attributes/default.rb
@@ -0,0 +1,4 @@
+# effectively override default google-chrome attributes
+
+default['google-chrome']['track'] = 'stable'
+# http://chromedriver.storage.googleapis.com/index.html?path=2.16/
---
+++ cookbooks/wrapper_google-chrome/metadata.rb
@@ -0,0 +1,7 @@
+name             'wrapper_google-chrome'
+maintainer       'Serguei Kouzmine'
+maintainer_email 'koumine_serguei@yahoo.com'
+description      'Installs/Configures chrome using cookbook wrapper'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
+depends	         'google-chrome' ,'>= 0.0.1'
