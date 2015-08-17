38f55f696db0099a166836f2a43c7bfeb68f1090
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-08-17 14:32:35 -0400

    moved into standalone file

--- cookbooks/custom_nuget/files/default/wget.ps1
+++ cookbooks/custom_nuget/files/default/wget.ps1
@@ -1,26 +1,49 @@
-# origin:
-# http://poshcode.org/4332
-# http://poshcode.org/4331
-param(
-  [string]$url,
-  [string]$local_path
+Param (
+  [string]$download_url,
+  [string]$local_file_path,
+  [string]$filename 
 )
 
-if (!(Split-Path -Parent $local_path) -or !(Test-Path -PathType Container (Split-Path -Parent $local_path))) {
-  $local_path = Join-Path $pwd (Split-Path -Leaf $local_path)
+
+function Get-ScriptDirectory {
+  $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+  if ($Invocation.PSScriptRoot) {
+    $Invocation.PSScriptRoot
+  }
+  elseif ($Invocation.MyCommand.Path) {
+    Split-Path $Invocation.MyCommand.Path
+  } else {
+    $Invocation.InvocationName.Substring(0,$Invocation.InvocationName.LastIndexOf(''))
+  }
 }
 
-if (-not (Test-Path $local_path))
+pushd $env:temp
+
+# support relative paths
+if (!(Split-Path -Parent $local_file_path) -or !(Test-Path -PathType Container (Split-Path -Parent $local_file_path))) {
+  $local_file_path = Join-Path (Get-ScriptDirectory) (Split-Path -Leaf $local_file_path)
+}
+
+if (-not (Test-Path $local_file_path))
 {
   throw "Folder ${local_path} does not Exist, file cannot be saved to"
 }
 
-Write-Output ("Downloading {0}`nSaving at {1}" -f $url,$local_path)
+$destination_file =  [System.IO.Path]::Combine($env:temp, ('downloaded_file_{0}.exe' -f (Get-Random -Maximum 5000) ))
+Write-Host ("Downloading {0}`nSaving as {1}" -f $download_url,$destination_file)
+
 $web_client = New-Object System.Net.WebClient
 # TODO: proxy authentication
 
-$web_client.DownloadFile($url,$local_path)
+$web_client.DownloadFile($download_url,$destination_file)
+# NOTE:
+# Exception calling "DownloadFile" with "2" argument(s): "An exception occurred during a WebClient request."
+# TODO: throw custom exception if unable to download the file.
 
-# TODO: throw exception if unable to download the file.
-
-
+Write-Host ("Moving {0} to {1}" -f $destination_file, $local_file_path)
+$local_file = [System.IO.Path]::Combine($local_file_path, $filename)
+copy-item -Destination $local_file -literalPath $destination_file
+if ( -not (test-path -path $local_file )) { 
+  Throw 'Failed to download file.'
+}
+popd
--- cookbooks/custom_nuget/recipes/default.rb
+++ cookbooks/custom_nuget/recipes/default.rb
@@ -13,52 +13,17 @@ CsQuery
 Newtonsoft.Json
 /
 
+cookbook_file "#{scripts_dir}/wget.ps1" do
+  source 'wget.ps1'
+  path "#{scripts_dir}/wget.ps1"
+  action :create_if_missing
+end
+
 powershell "Download #{package_title}" do
   code <<-EOH
 
-[string]$download_url = '#{download_url}'
-[string]$local_file_path = '#{temp_dir}'
-[string]$filename = '#{filename}'
-
-function Get-ScriptDirectory {
-  $Invocation = (Get-Variable MyInvocation -Scope 1).Value
-  if ($Invocation.PSScriptRoot) {
-    $Invocation.PSScriptRoot
-  }
-  elseif ($Invocation.MyCommand.Path) {
-    Split-Path $Invocation.MyCommand.Path
-  } else {
-    $Invocation.InvocationName.Substring(0,$Invocation.InvocationName.LastIndexOf(''))
-  }
-}
-
-# support relative paths
-if (!(Split-Path -Parent $local_file_path) -or !(Test-Path -PathType Container (Split-Path -Parent $local_file_path))) {
-  $local_file_path = Join-Path (Get-ScriptDirectory) (Split-Path -Leaf $local_file_path)
-}
-
-if (-not (Test-Path $local_file_path))
-{
-  throw "Folder ${local_path} does not Exist, file cannot be saved to"
-}
-
-$destination_file =  ('downloaded_file_{0}.exe' -f (Get-Random -Maximum 5000) )
-Write-Host ("Downloading {0}`nSaving as {1}" -f $download_url,$destination_file)
-
-$web_client = New-Object System.Net.WebClient
-# TODO: proxy authentication
-
-$web_client.DownloadFile($download_url,$destination_file)
-# NOTE:
-# Exception calling "DownloadFile" with "2" argument(s): "An exception occurred during a WebClient request."
-# TODO: throw custom exception if unable to download the file.
-
-Write-Host ("Moving {0} to {1}" -f $destination_file, $local_file_path)
-$local_file = [System.IO.Path]::Combine($local_file_path, $filename)
-copy-item -Destination $local_file -literalPath $destination_file
-if ( -not (test-path -path $local_file )) { 
-  Throw 'Failed to download file.'
-}
+  pushd '#{temp_dir}'
+ . .\\wget.ps1 -download_url '#{download_url}' -local_file_path '#{temp_dir}' -filename '#{filename}'
 
   EOH
   not_if {::File.exists?("#{temp_dir}/#{filename}".gsub('\\', '/'))}
@@ -86,6 +51,7 @@ end
 cookbook_file "#{scripts_dir}/get_task_scheduler_events.ps1" do
   source 'get_task_scheduler_events.ps1'
   path "#{scripts_dir}/get_task_scheduler_events.ps1"
+  action :create_if_missing
 end
 
 powershell 'Execute uploaded script' do
