95ecd2a6834a70f252c124761f3e30108cd4edb4
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-02-07 09:22:34 -0500

    remove the verbatim password, obsolete but nevertheless

--- launch-vagrant.cmd
+++ launch-vagrant.cmd
@@ -3,6 +3,6 @@ cd %~dp0
 set VAGRANT_HOME=%CD%
 set VAGRANT_USE_PROXY=1
 set PATH=%VAGRANT_HOME%\bin;%VAGRANT_HOME%\embedded\bin;%PATH%
-set HTTP_PROXY=http://sergueik:Pe%%40rlF1sher%%3B@proxy.carnival.com:8080
+set HTTP_PROXY=http://sergueik:password@proxy.carnival.com:8080
 
 goto :EOF
