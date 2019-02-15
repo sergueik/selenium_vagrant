d7f66fb901aa5acf289c4f501231ea24d81172ab
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-02-14 19:35:49 -0500

    intermediate revision - documented inline and made  parameter name self explanatory, also added a number of parameters

--- new/cookbooks/unix_sample/attributes/default.rb
+++ new/cookbooks/unix_sample/attributes/default.rb
@@ -1,9 +1,18 @@
-# TODO: add user cookbook
-default['purge']['track'] = '0.2'
-default['purge']['basedir'] = '/home/vagrant' # '/opt/jenkins'
-default['purge']['account_username'] = 'vagrant'
-default['purge']['critical_percent'] = '5'
-default['purge']['mount'] = 'vagrant'
-default['purge']['password'] = 'password'
+default['target_node']['script_version'] = '0.3'
+
+
+# @basedir is the parent dir of .m2 e.g. for prod: '/opt/jenkins'
+default['target_node']['basedir'] = '/home/vagrant' 
+# the high disk usage of @disk would trigger the purge script
+default['target_node']['disk'] = '/dev/sda1' 
+# Bash script 'purge.sh' is created under account_username home directory 
+default['target_node']['account_username'] = 'vagrant' 
+# the high disk usage over @high_percent is considered high
+default['target_node']['high_percent'] = '3'
+# when do_purge is set to true, the bash script `purge.sh` is run in every
+# chef run, otherwise it will examine the df output for filesystem mounted to @mount_dir
+default['target_node']['do_purge'] = true
+default['target_node']['mount_dir'] = 'vagrant'
+default['target_node']['password'] = 'password'
 
 
--- new/cookbooks/unix_sample/recipes/default.rb
+++ new/cookbooks/unix_sample/recipes/default.rb
@@ -1,30 +1,34 @@
-log "Installing custom purge scripts version #{node['purge']['track']}" do
+log "Installing custom purge scripts version #{node['target_node']['script_version']}" do
   level :info
 end
 
-# TODO: install java and maven to use maven command to purge stuff
-# can also  brute force delete .m2/repositories, of course
-# https://stackoverflow.com/questions/7408545/how-do-you-clear-apache-mavens-cache
-# include_recipe 'jenkins::java'
-# include_recipe 'jenkins::master'
-
-# Define variables for attributes
-use_default_version = false
-account_username = node['purge']['account_username']
-basedir = node['purge']['basedir']
-critical_percent = node['purge']['critical_percent'].to_i
-mount = node['purge']['mount']
+account_username = node['target_node']['account_username']
+basedir = node['target_node']['basedir']
+high_percent = node['target_node']['high_percent'].to_i
+mount_dir = node['target_node']['mount_dir']
+disk = node['target_node']['disk']
+if disk.nil?
+  disk = '/dev/sda1'
+end
+do_purge = node['target_node']['do_purge']
 account_home = "/home/#{account_username}"
 purge_script = 'purge.sh'
 debug = false
 
 begin
+  # https://www.linuxnix.com/chef-get-node-attributes-or-values/
+  # https://www.jvt.me/posts/2018/08/29/debugging-chef-shell/
+  # # ohai filesystem/by_mount_dirpoint
+  # # chef-shell
+  # attributes_mode
+  # chef:attributes > attributes[:filesystem]['by_device']['/dev/sda1']['percent_used'].to_i
+
   if node.attribute?('filesystem')
     log "Attribute filesysem is available" do
       level :info
     end
     fs = node['filesystem']
-    percent_used = fs['by_device']['/dev/sda1']['percent_used'].to_i
+    percent_used = fs['by_device'][disk]['percent_used'].to_i
     scratch_obj = fs.to_s
   else
     log "Attribute filesysem is not available" do
@@ -45,9 +49,7 @@ rescue => e
     level :info
   end
 end
-# need to utilize ohai attribute node['/dev/xvda1']['kb_size'] instead of
-# calling df directly in the shell script
-if percent_used > critical_percent
+if percent_used > high_percent
   directory "#{account_home}/scripts" do
     owner account_username
     group account_username
@@ -59,10 +61,11 @@ if percent_used > critical_percent
   template ("#{account_home}/scripts/#{purge_script}") do
     source 'purge.erb'
     variables(
-      :critical_percent => critical_percent,
-      :mount            => mount,
-      :basedir          => basedir,
-      :ipaddress        => node['ipaddress'],
+      :high_percent => high_percent,
+      :mount_dir    => mount_dir,
+      :do_purge     => do_purge,
+      :basedir      => basedir,
+      :ipaddress    => node['ipaddress'],
       )
     owner account_username
     group account_username
@@ -79,6 +82,10 @@ if percent_used > critical_percent
       ignore_failure true
       only_if { ::File.exists?("#{account_home}/scripts/#{purge_script}") }
   end
+else
+log "The disk usage #{percent_used} is < {high_percent}." do
+  level :info
+end
 end
 log 'Finished configuring Node.' do
   level :info
