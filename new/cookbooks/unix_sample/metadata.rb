0e895c31ee1c35805271dab379c5e63ea5d62219
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-02-19 20:53:07 -0500

    intermediate revision - finished collapsing windows and unix cookbook directories, verified on  Linux host

--- new/cookbooks/unix_sample/metadata.rb
+++ new/cookbooks/unix_sample/metadata.rb
@@ -8,12 +8,13 @@ version          '0.0.4'
 supports         'ubuntu', '>= 12.04'
 supports         'centos'
 supports         'windows'
+# on Windows host still need to comment 'windows' and 'powershell' dependencies
 #  linux: Recipe Compile Error in /var/chef/cache/cookbooks/powershell/libraries/powershell_module_provider.rb
 # ==> linux: ================================================================================
 # ==> linux: ArgumentError
 # ==> linux: -------------
 # ==> linux:
 # ==> linux: unknown keyword: on_platforms
-# depends          'windows', '>= 5.2.3'
-# depends          'powershell', '>= 6.1.3'
+depends          'windows', '>= 5.2.3'
+depends          'powershell', '>= 6.1.3'
 depends	         'jenkins' ,'>= 6.2.0'
